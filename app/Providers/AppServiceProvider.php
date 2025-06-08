71462006022980bfa7fd6386847f3eee06dc672c
AdrianJMC <derrapo67@gmail.com>
2025-06-07 23:15:33 -0600

    utlimo intento alv

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
 use Spatie\Permission\Middleware\RoleMiddleware;
-
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,11 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') !== 'local' && isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            URL::forceScheme('https');
+        }
+
         // Forzar el registro si Laravel no lo reconoce
         Route::aliasMiddleware('role', RoleMiddleware::class);
 
         Blade::component('components.info-card', 'info-card');
-
-        
     }
 }
